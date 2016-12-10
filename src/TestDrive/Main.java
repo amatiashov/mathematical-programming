c98f664b8635276980020e7f22b0e91700e50532
Artyom Matyashov <smart7797@gmail.com>
2016-12-10 22:13:07 +0300

    Add support for input of negative number in enterNumber method

--- src/TestDrive/Main.java
+++ src/TestDrive/Main.java
@@ -36,6 +36,10 @@ public class Main {
         boolean firstValueIsZero = true;
         for (int i = 0; i < tmp.length(); i++){
             Character c = tmp.charAt(i);
+            if (i == 0 && c == '-'){
+                number.append(c);
+                continue;
+            }
             if (!Character.isDigit(c)){
                 throw new RuntimeException("Enter value is not number");
             }
